ec7d591f25bfd0d4fc99523335a1d3bd9c3f6a02
rahul <murahul76@gmail.com>
2025-11-07 13:09:00 +0530

    re

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Replace '<REPOSITORY_NAME>' with the actual name of your repository
+const repoName = '/news-website/'; 
+
 export default defineConfig({
   plugins: [react()],
-})
+  // Set the base path for deployment
+  base: process.env.NODE_ENV === 'production' ? repoName : '/',
+  // OR, for a simpler fix that generally works:
+  // base: './', 
+});
