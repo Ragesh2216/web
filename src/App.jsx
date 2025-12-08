ccc51081e7978fb8f9c88ae02ffa427f734e098e
rahul <murahul76@gmail.com>
2025-12-08 09:36:53 +0530

    vnn

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,14 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import Scrolltop from "./components/Scrolltop"; // Fixed typo in component name
+import Scrooltop from "./components/Scrooltop"; // Note the correct filename
 import './App.css';
 
 function App({ element }) {
-  const location = useLocation();
-
-  // Scroll to top on route change with hash router
-  useEffect(() => {
-    // For hash router, we need to handle scroll differently
-    if (location.hash) {
-      // If there's a hash in the URL (e.g., #section), let the browser handle it
-      setTimeout(() => {
-        const element = document.getElementById(location.hash.replace('#', ''));
-        if (element) {
-          element.scrollIntoView({ behavior: 'smooth' });
-        }
-      }, 100);
-    } else {
-      // Scroll to top for regular route changes
-      window.scrollTo(0, 0);
-    }
-  }, [location.pathname, location.hash]);
-
   return (
     <>
-      <Scrolltop />
+      <Scrooltop/> {/* Use the correct component name */}
       <Navbar />
-      <main>
-        {element}
-      </main>
+      {element}
       <Footer />
     </>
   );
